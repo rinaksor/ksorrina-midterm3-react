cea178787fc8154207ee6c892fc84b51031e26fe
rinaksor <146155132+rinaksor@users.noreply.github.com>
2024-05-23 01:07:30 +0700

    found and not found

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import React from "react";
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
 import Search from "./components/users/Search";
+import About from "./components/users/About";
+import NotFound from "./components/users/NotFound";
+
 const App = () => {
-return (
-<div className="App">
-<Navbar />
-<div className="container">
-<h1>GitHub Users Data</h1>
-<Search />
-</div>
-</div>
-);
-};
-export default App;
+  return (
+    <div className="App">
+    <Router>
+    <Navbar />
+    <div className="container">
+    <Switch>
+    <Route exact path="/" component={Search} />
+    <Route exact path="/about" component={About} />
+    <Route path="/*" component={NotFound}></Route>
+    </Switch>
+    </div>
+    </Router>
+    </div>
+    );
+    };
+    export default App;
