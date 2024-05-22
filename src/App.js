493451011fc02abf1c8a09796d0c312841617614
rinaksor <146155132+rinaksor@users.noreply.github.com>
2024-05-23 00:56:42 +0700

    done search

--- src/App.js
+++ src/App.js
@@ -1,30 +1,16 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React from "react";
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
-import Users from "./components/users/Users";
-
+import Search from "./components/users/Search";
 const App = () => {
-  const [users, setUsers] = useState([]);
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get("https://api.github.com/users");
-        setUsers(response.data);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    };
-    fetchData();
-  }, []);
-  return (
-    <div className="App">
-      <Navbar />
-      <div className="container">
-        <h1>GitHub Users Data</h1>
-      </div>
-      <Users users={users} />
-    </div>
-  );
+return (
+<div className="App">
+<Navbar />
+<div className="container">
+<h1>GitHub Users Data</h1>
+<Search />
+</div>
+</div>
+);
 };
 export default App;
