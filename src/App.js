35de017a8d561bb732f2bc3e25fd98f07779c1ff
rinaksor <146155132+rinaksor@users.noreply.github.com>
2024-05-23 00:54:51 +0700

    fetch data thanh cong

--- src/App.js
+++ src/App.js
@@ -1,32 +1,30 @@
+import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { useEffect } from "react";
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
-function App() {
-  // Use the 'useEffect' hook to perform side effects in function components
+import Users from "./components/users/Users";
+
+const App = () => {
+  const [users, setUsers] = useState([]);
   useEffect(() => {
-    // Define an asynchronous function 'fetchData' to fetch data from the GitHub API
     const fetchData = async () => {
       try {
-        // Use the 'axios' library to make a GET request to the GitHub API endpoint
         const response = await axios.get("https://api.github.com/users");
-        // Log the fetched data to the console
-        console.log("GitHub Users:", response.data);
+        setUsers(response.data);
       } catch (error) {
-        // Log an error message if there's an issue fetching data
         console.error("Error fetching data:", error);
       }
     };
-    // Call the 'fetchData' function when the component mounts
     fetchData();
-  }, []); // The empty dependency array ensures that 'useEffect' runs only once when the component mounts
+  }, []);
   return (
     <div className="App">
       <Navbar />
       <div className="container">
-        <h1>Hello from React</h1>
+        <h1>GitHub Users Data</h1>
       </div>
+      <Users users={users} />
     </div>
   );
-}
+};
 export default App;
