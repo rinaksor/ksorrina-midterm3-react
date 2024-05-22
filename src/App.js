dcf9ef37881410f13aca45ae42af8848ab26fde9
rinaksor <146155132+rinaksor@users.noreply.github.com>
2024-05-23 01:32:05 +0700

    create all folder

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 import React from "react";
-import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
+import Home from "./components/layout/Home";
 import Navbar from "./components/layout/Navbar";
-import Search from "./components/users/Search";
-import About from "./components/users/About";
-import NotFound from "./components/users/NotFound";
-
 const App = () => {
   return (
     <div className="App">
-    <Router>
-    <Navbar />
-    <div className="container">
-    <Switch>
-    <Route exact path="/" component={Search} />
-    <Route exact path="/about" component={About} />
-    <Route path="/*" component={NotFound}></Route>
-    </Switch>
+      <Router>
+        <Navbar />
+        <Home />
+      </Router>
     </div>
-    </Router>
-    </div>
-    );
-    };
-    export default App;
+  );
+};
+export default App;
--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,5 +1,5 @@
-// /src/components/users/UserItem.js
 import React from "react";
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
 const UserItem = (props) => {
   const { login, avatar_url, html_url } = props.user;
   return (
@@ -12,9 +12,9 @@ const UserItem = (props) => {
       />
       <h3>{login}</h3>
       <div>
-        <a href="#" className="btn btn-dark btn-sm my-1">
+        <Link to={`/user/${login}`} className="btn btn-dark btn-sm my-1">
           More
-        </a>
+        </Link>
       </div>
     </div>
   );
