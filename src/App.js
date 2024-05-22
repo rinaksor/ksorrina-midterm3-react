95306ddb9f22ec50184d86929f62fed68f439cfa
rinaksor <146155132+rinaksor@users.noreply.github.com>
2024-05-23 00:43:29 +0700

    done setup folder

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-
+import axios from "axios";
+import { useEffect } from "react";
+import "./App.css";
+import Navbar from "./components/layout/Navbar";
 function App() {
+  // Use the 'useEffect' hook to perform side effects in function components
+  useEffect(() => {
+    // Define an asynchronous function 'fetchData' to fetch data from the GitHub API
+    const fetchData = async () => {
+      try {
+        // Use the 'axios' library to make a GET request to the GitHub API endpoint
+        const response = await axios.get("https://api.github.com/users");
+        // Log the fetched data to the console
+        console.log("GitHub Users:", response.data);
+      } catch (error) {
+        // Log an error message if there's an issue fetching data
+        console.error("Error fetching data:", error);
+      }
+    };
+    // Call the 'fetchData' function when the component mounts
+    fetchData();
+  }, []); // The empty dependency array ensures that 'useEffect' runs only once when the component mounts
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <div className="container">
+        <h1>Hello from React</h1>
+      </div>
     </div>
   );
 }
-
 export default App;
